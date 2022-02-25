5695f4026370068090d6e93ef7828be8a7f6126f
LBXgitHubb <bing6448@gmail.com>
2022-02-26 03:05:15 +0800

    added NewTestjava for selenium testing

--- TouristMe/src/test/java/TouristMe/NewTest.java
+++ TouristMe/src/test/java/TouristMe/NewTest.java
@@ -2,11 +2,18 @@ package TouristMe;
 
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 
 public class NewTest {
   @Test
   public void f() {
+	  System.setProperty("webdriver.chrome.driver","C:\\Program Files (x86)\\Google\\Chrome\\chromedriver.exe");
+	  WebDriver driver = new ChromeDriver();
+	  //driver.get();
+	  
+	  //click on login enter the user and password and go back to check the login status
   }
   @BeforeTest
   public void beforeTest() {
