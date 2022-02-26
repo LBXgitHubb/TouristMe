1602e3a34f57eec448283eb8400edd0690eca998
LBXgitHubb <bing6448@gmail.com>
2022-02-26 16:59:00 +0800

    updated NewTest

--- TouristMe/src/test/java/TouristMe/NewTest.java
+++ TouristMe/src/test/java/TouristMe/NewTest.java
@@ -2,6 +2,7 @@ package TouristMe;
 
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
@@ -11,9 +12,16 @@ public class NewTest {
   public void f() {
 	  System.setProperty("webdriver.chrome.driver","C:\\Program Files (x86)\\Google\\Chrome\\chromedriver.exe");
 	  WebDriver driver = new ChromeDriver();
-	  //driver.get();
 	  
-	  //click on login enter the user and password and go back to check the login status
+	  driver.get("http://localhost:8090/TouristMe/ShowListing");  
+	  driver.findElement(By.linkText("Login")).click();
+	  driver.get("http://localhost:8090/TouristMe/ShowListing");  
+	  driver.findElement(By.linkText("Register")).click();
+	  driver.get("http://localhost:8090/TouristMe/ShowListing");  
+	  driver.findElement(By.linkText("Booking")).click();
+	  driver.get("http://localhost:8090/TouristMe/ShowListing"); 
+	  
+	  //check if the routing is working correctly
   }
   @BeforeTest
   public void beforeTest() {
